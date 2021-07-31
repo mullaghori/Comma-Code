d54b6b8e60e382c4d66d761f18ae0217b0b4e01e
mullaghori <87578912+mullaghori@users.noreply.github.com>
2021-07-31 11:22:59 +0500

    Add files via upload

---
+++ picture.grid.py
@@ -0,0 +1,33 @@
+
+
+
+grid = [
+['.', '.', '.', '.', '.', '.'],
+['.', 'o', 'o', '.', '.', '.'],
+['o', 'o', 'o', 'o', '.', '.'],
+['o', 'o', 'o', 'o', 'o', '.'],
+['.', 'o', 'o', 'o', 'o', 'o'],
+['o', 'o', 'o', 'o', 'o', '.'],
+['o', 'o', 'o', 'o', '.', '.'],
+['.', 'o', 'o', '.', '.', '.'],
+['.', '.', '.', '.', '.', '.']
+]
+
+
+for items in grid:
+	print(items[0], end='')
+print('')
+for items in grid:
+	print(items[1], end='')
+print('')
+for items in grid:
+	print(items[2], end='')
+print('')
+for items in grid:
+	print(items[3], end='')
+print('')
+for items in grid:
+	print(items[4], end='')
+print('')
+for items in grid:
+	print(items[5], end='')
---
+++ picture_grid2.py
@@ -0,0 +1,21 @@
+
+
+
+grid = [
+['.', '.', '.', '.', '.', '.'],
+['.', 'o', 'o', '.', '.', '.'],
+['o', 'o', 'o', 'o', '.', '.'],
+['o', 'o', 'o', 'o', 'o', '.'],
+['.', 'o', 'o', 'o', 'o', 'o'],
+['o', 'o', 'o', 'o', 'o', '.'],
+['o', 'o', 'o', 'o', '.', '.'],
+['.', 'o', 'o', '.', '.', '.'],
+['.', '.', '.', '.', '.', '.']
+]
+
+print(grid[0][0]+grid[1][0]+grid[2][0]+grid[3][0]+grid[4][0]+grid[5][0]+grid[6][0]+grid[7][0]+grid[8][0])
+print(grid[0][1]+grid[1][1]+grid[2][1]+grid[3][1]+grid[4][1]+grid[5][1]+grid[6][1]+grid[7][1]+grid[8][1])
+print(grid[0][2]+grid[1][2]+grid[2][2]+grid[3][2]+grid[4][2]+grid[5][2]+grid[6][2]+grid[7][2]+grid[8][2])
+print(grid[0][3]+grid[1][3]+grid[2][3]+grid[3][3]+grid[4][3]+grid[5][3]+grid[6][3]+grid[7][3]+grid[8][3])
+print(grid[0][4]+grid[1][4]+grid[2][4]+grid[3][4]+grid[4][4]+grid[5][4]+grid[6][4]+grid[7][4]+grid[8][4])
+print(grid[0][5]+grid[1][5]+grid[2][5]+grid[3][5]+grid[4][5]+grid[5][5]+grid[6][5]+grid[7][5]+grid[8][5])
